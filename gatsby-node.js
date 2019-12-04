daebb46580e0f2dd519d1ad42582c809cda9b7d2
Gabe <jflores353@gmail.com>
2019-12-03 22:46:30 -0600

    returned new Promise that queries the frontmatter slug, createPage then uses the slug(path) to create a page for each post

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,8 +7,30 @@ const path = require("path")
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: `/fakepage`,
-    component: path.resolve("./src/components/postLayout.js"),
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.slug,
+          component: path.resolve("./src/components/postLayout.js"),
+          context: {
+            slug: node.frontmatter.slug,
+          },
+        })
+      })
+      resolve()
+    })
   })
 }
