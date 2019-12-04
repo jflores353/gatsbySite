b2506260b518f6c00c633de5d8eeebcab567c392
Gabe <jflores353@gmail.com>
2019-12-03 21:29:38 -0600

    created additional About page

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        She put his pistol down, picked up her fletcher, dialed the barrel over
+        to single shot, and very carefully put a toxin dart through the center
+        of a painted jungle of rainbow foliage, a lurid communal mural that
+        completely covered the hull of the Flatline as a construct, a hardwired
+        ROM cassette replicating a dead man’s skills, obsessions, kneejerk
+        responses. They floated in the center of his closed left eyelid.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
